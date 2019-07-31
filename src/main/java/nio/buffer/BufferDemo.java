edf51e3bc276e1557a4aed2da7f4b2531d40c783
laixiaoxing <291826425@qq.com>
2019-07-31 12:52:05 +0800

    duplicate和slice的用法

--- src/main/java/nio/buffer/BufferDemo.java
+++ src/main/java/nio/buffer/BufferDemo.java
@@ -94,10 +94,9 @@ public class BufferDemo {
         }
 
 
-
         //放入字符串 要先用flip()将postion位置重置回写
         buffer.flip();
-        String s="lai";
+        String s = "lai";
         buffer.put(s);
         //原来是CDCEF 先变成了laiEF 而且输出是从EF开始输出的
         System.out.println("放入字符串");
@@ -113,16 +112,15 @@ public class BufferDemo {
         }
 
 
-
         System.out.println("指定放入字符串的某个片段");
         buffer.flip();
-        buffer.put(s,0,1);
+        buffer.put(s, 0, 1);
         while (buffer.hasRemaining()) {
             System.out.println(buffer.get());
         }
 
 
-      //  flip()   用于切换读写模式， 比如说 我写入了1，2，3 此时position的位置就是3，然后我要读，就要flip 一下，此时的position就变成了0，limit变成了3
+        //  flip()   用于切换读写模式， 比如说 我写入了1，2，3 此时position的位置就是3，然后我要读，就要flip 一下，此时的position就变成了0，limit变成了3
         CharBuffer b2 = CharBuffer.allocate(100);
         b2.put('1');
         b2.put('2');
@@ -154,9 +152,50 @@ public class BufferDemo {
         }
 
         // position和limit都是可以重新设定的
-//        b2.position(1);
-//        b2.limit(1);
-//        b2.capacity();
+        //        b2.position(1);
+        //        b2.limit(1);
+        //        b2.capacity();
+
+
+        // 新的缓冲区是由分配或包装操作创建的。分配操作创建一个缓冲区对象并分配一个私有的 空间来储存容量大小的数据元素
+        //里面是一个数组
+        CharBuffer charBuffer = CharBuffer.allocate(100);
+
+
+        //自己的数组用做缓冲区的备份存储器
+        char[] myArray = new char[100];
+        CharBuffer charbuffer = CharBuffer.wrap(myArray);
+        charbuffer.put('A');
+        myArray[2] = 'B';
+        //这段代码构造了一个新的缓冲区对象，但数据元素会存在于数组中。这意味着通过调用 put()函数造成的对缓冲区的改动会直接影响这个数组，
+        // 而且对这个数组的任何改动也会对这 个缓冲区对象可见
+
+        //        Boolean 型函数 hasArray()告诉您这个缓冲区是否有 一个可存取的备份数组。如果这个函数的返回 true，array()函数会返回这个缓冲区对象所 使用的数组存储空间的引用。
+        //        如果 hasArray()函数返回 false，不要调用 array()函数或者 arrayOffset()函 数。如果您这样做了您会得到一个 UnsupportedOperationException
+
+
+        //将已有的字符串变成缓冲区
+        //  CharBuffer charBuffer = CharBuffer.wrap ("Hello World");
+
+
+
+        //复制
+        //Duplicate()函数创建了一个与原始缓冲区相似的新缓冲区。两个缓冲区共享数据元
+        //素，拥有同样的容量，但每个缓冲区拥有各自的位置，上界和标记属性。对一个缓冲区内的数 据元素所做的改变会反映在另外一个缓冲区上。这一副本缓冲区具有与原始缓冲区同样的数据 视图。如果原始的缓冲区为只读，或者为直接缓冲区，新的缓冲区将继承这些属性
+        CharBuffer b3=b2.duplicate();
+        b3.position(2);
+        b3.get();
+
+
+
+        //        复制一个缓冲区会创建一个新的 Buffer 对象，但并不 复制数据。原始缓冲区和副本都会操作同样的数据元 素。
+
+
+        //  slice()
+        //        slice()创建一个从原始缓冲区的当前位置开始的新缓冲 区，并且其容量是原始缓冲区的剩余元素数量(limit-position)。这个新缓冲区与原始 缓冲区共享一段数据元素子序列。分割出来的缓冲区也会继承只读和直接属性。
+        b2.limit(7);
+        CharBuffer b4=b2.slice();
+        b4.get();
 
 
     }
---
+++ src/main/java/nio/buffer/ByteBufferDemo.java
@@ -0,0 +1,12 @@
+package nio.buffer;
+
+/**
+ * @ClassName ByteBufferDemo
+ * @Author laixiaoxing
+ * @Date 2019/7/31 下午12:50
+ * @Description 字节缓冲区
+ * @Version 1.0
+ */
+public class ByteBufferDemo {
+    
+}
