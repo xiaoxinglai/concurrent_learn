4962933b46877be243552c2efb1bc07332b6037a
laixiaoxing <291826425@qq.com>
2019-07-31 10:29:41 +0800

    nio的buffer的flip和position以及limit的理解

--- src/main/java/nio/buffer/BufferDemo.java
+++ src/main/java/nio/buffer/BufferDemo.java
@@ -95,8 +95,68 @@ public class BufferDemo {
 
 
 
+        //放入字符串 要先用flip()将postion位置重置回写
+        buffer.flip();
+        String s="lai";
+        buffer.put(s);
+        //原来是CDCEF 先变成了laiEF 而且输出是从EF开始输出的
+        System.out.println("放入字符串");
+        while (buffer.hasRemaining()) {
+            System.out.println(buffer.get());
+        }
+
+        //读之前要flip刷新位置
+        System.out.println("放入字符串之后刷新位置 切换模式");
+        buffer.flip();
+        while (buffer.hasRemaining()) {
+            System.out.println(buffer.get());
+        }
 
 
+
+        System.out.println("指定放入字符串的某个片段");
+        buffer.flip();
+        buffer.put(s,0,1);
+        while (buffer.hasRemaining()) {
+            System.out.println(buffer.get());
+        }
+
+
+      //  flip()   用于切换读写模式， 比如说 我写入了1，2，3 此时position的位置就是3，然后我要读，就要flip 一下，此时的position就变成了0，limit变成了3
+        CharBuffer b2 = CharBuffer.allocate(100);
+        b2.put('1');
+        b2.put('2');
+        //此时limit 100 position  2   capacity为100
+        b2.flip();
+        //此时limit 2  position  0   capacity为100
+
+        //此时如果用put 会覆盖之前的 1 2
+        b2.put('3');
+        b2.put('4');
+        b2.flip();
+        while (b2.hasRemaining()) {
+            System.out.println(b2.get());
+        }
+
+        //此时limit 为2 position为2 ，如果此时直接put 会因为已经到了limit最大值而异常  b2.put('5'); java.nio.BufferOverflowException
+        //如果改成flip 则又回到了一开始的读模式，从position为0的位置开始写  会覆盖之前的
+
+
+        //如果要追加
+        //要先修改limit 然后用put或者apend
+        //flip是要求先写再读，插入在写和读之间，如果flip flip 就等于中间有一次没写就读了， 当然是就会是position为0
+        System.out.println("追加5");
+        b2.limit(b2.capacity());
+        b2.append('5');
+        b2.flip();
+        while (b2.hasRemaining()) {
+            System.out.println(b2.get());
+        }
+
+        // position和limit都是可以重新设定的
+//        b2.position(1);
+//        b2.limit(1);
+//        b2.capacity();
 
 
     }
