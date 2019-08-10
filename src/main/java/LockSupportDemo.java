0e16b6196b4239a6932a5416cbd3b1c8ab33709b
laixiaoxing <291826425@qq.com>
2019-08-10 20:11:54 +0800

    LockSupport的使用

---
+++ src/main/java/LockSupportDemo.java
@@ -0,0 +1,23 @@
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * @ClassName LockSupportDemo
+ * @Author laixiaoxing
+ * @Date 2019/8/10 下午4:21
+ * @Description LockSupport工具类
+ * @Version 1.0
+ */
+public class LockSupportDemo {
+
+    public  void testPark(){
+        LockSupport.park(this);
+    }
+
+
+    public  static void main(String[] args) {
+        LockSupportDemo lockSupportDemo=new LockSupportDemo();
+        lockSupportDemo.testPark();
+        Thread
+
+    }
+}
